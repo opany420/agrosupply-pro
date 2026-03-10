401bac943148c1b02c844128e90bdbb5df596071
Felix <felixopany420@gmail.com>
2026-03-10 19:15:24 +0300

    Fix Google Maps embed - no API key

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -158,14 +158,14 @@ export default function Contact() {
               {/* Google Map Embed - Ahero, Kisumu */}
               <div className="rounded-2xl overflow-hidden shadow-lg h-64 mb-8 border border-gray-100">
                 <iframe
-                  title="Chicago Agro Supplies Location"
-                  width="100%"
-                  height="100%"
-                  style={{ border: 0 }}
-                  loading="lazy"
-                  allowFullScreen
-                  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY&q=Ahero,Kisumu,Kenya">
-                </iframe>
+  title="Chicago Agro Supplies Location"
+  width="100%"
+  height="100%"
+  style={{ border: 0 }}
+  loading="lazy"
+  allowFullScreen
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.4!2d34.9197!3d-0.1667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182aa3b0b0b0b0b0%3A0x0!2sAhero%2C+Kisumu%2C+Kenya!5e0!3m2!1sen!2ske!4v1620000000000!5m2!1sen!2ske">
+</iframe>
               </div>
 
               {/* Payment Info */}

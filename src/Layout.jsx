e3f1e64efaa36fa5726cd88ade2b64ca21517143
Felix <felixopany420@gmail.com>
2026-03-12 13:50:23 +0300

    Fix accessibility - add skip-to-content link in Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -32,6 +32,7 @@ export default function Layout({ children }) {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-emerald-600 focus:text-white focus:rounded">Skip to main content</a>
       <Cart />
 
       {/* WhatsApp Floating Button */}
@@ -157,7 +158,7 @@ export default function Layout({ children }) {
       </header>
 
       {/* Main Content */}
-      <main className="flex-1">{children}</main>
+      <main id="main-content" className="flex-1">{children}</main>
 
       <ChatWidget />
 
